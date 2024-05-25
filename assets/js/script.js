dd6e0f969ffe20c3f6dc2485fdace727990a2d6b
Sebastian Bucher <Sebasti.b@web.de>
2024-05-25 19:17:07 +0000

    feat: add postForm function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,17 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    for (let entry of form.entries) {
+        console.log(entry)
+    }
+}
 
 async function getStatus(e) {
-
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -17,7 +25,6 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
-
 }
 
 function displayStatus(data) {
