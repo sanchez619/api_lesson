4eb97a5fa4ee1fc656285ddf6498b11ecab809e0
Sebastian Bucher <Sebasti.b@web.de>
2024-05-25 18:24:52 +0000

    fix: add numbers 123 to check error statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "sYw31UkWw0dmCPqws09YSkcNXS4";
+const API_KEY = "sYw31UkWw0dmCPqws09YSkcNXS4123";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
